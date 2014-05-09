3bf0ffabef6d97d4c92718c1a07df927ad833597
Alps <alps@alps1992.local>
2014-05-09 16:00:57 +0800

    done RssiSend

--- ep3/BlinkToRadio.h
+++ ep3/BlinkToRadio.h
@@ -3,12 +3,15 @@
 
 enum{
 	AM_BLINKTORADIO=6;
-	TIMER_PERIOD_MILLI = 250;
+	TIMER_PERIOD_MILLI = 500;
+	SET_POWER = 15;
 };
 
 typedef nx_struct BlinkToRadioMsg{
 	nx_uint16_t node_id;
-	nx_uint16_t counter;
+	nx_uint8_t power;
+	nx_int8_t rssi;
+	nx_uint8_t lqi;
 } BlinkToRadioMsg;
 
 #endif
